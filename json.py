273c1bec5c55f737c4115b433155c4f56932bee1
adenhou86 <76795165+adenhou86@users.noreply.github.com>
2025-02-17 10:23:57 -0500

    Create json.py

---
+++ json.py
@@ -0,0 +1,146 @@
+{
+    "Q1": {
+        "Proceeds from Sales": {
+            "HTM": null,
+            "AFS": 3020
+        },
+        "Proceeds from Paydowns and Maturities": {
+            "HTM": 5283,
+            "AFS": 7476
+        },
+        "Purchases": {
+            "HTM": -7000,
+            "AFS": -16850
+        },
+        "Net": {
+            "AFS": -6354,
+            "HTM": 5283
+        }
+    },
+    "Q2": {
+        "Proceeds from Sales": {
+            "HTM": null,
+            "AFS": 4671
+        },
+        "Proceeds from Paydowns and Maturities": {
+            "HTM": 5671,
+            "AFS": 2313
+        },
+        "Purchases": {
+            "HTM": -6433,
+            "AFS": -10886
+        },
+        "Net": {
+            "AFS": -3902,
+            "HTM": -762
+        }
+    },
+    "Q3": {
+        "Proceeds from Sales": {
+            "HTM": null,
+            "AFS": 5696
+        },
+        "Proceeds from Paydowns and Maturities": {
+            "HTM": 1644,
+            "AFS": 2995
+        },
+        "Purchases": {
+            "HTM": -7227,
+            "AFS": -25191
+        },
+        "Net": {
+            "AFS": -16500,
+            "HTM": -5583
+        }
+    },
+    "Securities": {
+        "AFS": {
+            "Q1": 36895,
+            "Q2": 38051,
+            "Q3": 38950,
+            "Change in amort cost of Agency RMBS": {
+                "Q1": [
+                    38501,
+                    1606,
+                    21768,
+                    23722
+                ],
+                "Q2": [
+                    38950,
+                    449,
+                    23722,
+                    30251
+                ],
+                "Q3": [
+                    37935,
+                    -1015,
+                    30751,
+                    31201
+                ]
+            },
+            "Change in amort cost of USTs": {
+                "Q1": [
+                    1954,
+                    6713,
+                    6218
+                ],
+                "Q2": [
+                    6529,
+                    6218,
+                    6757
+                ],
+                "Q3": [
+                    5950,
+                    6595,
+                    6757
+                ]
+            },
+            "Change in amort cost in ABS": {
+                "Q1": [
+                    -495,
+                    10768
+                ],
+                "Q2": [
+                    377,
+                    10826
+                ],
+                "Q3": [
+                    162,
+                    10808
+                ]
+            },
+            "Change in amort cost munis": {
+                "Q1": 58,
+                "Q2": -18,
+                "Q3": -76
+            }
+        },
+        "HTM": {
+            "Q1": 82692,
+            "Q2": 81502,
+            "Q3": 80027,
+            "Change in amort cost RMBS": {
+                "Q1": [
+                    81502,
+                    -1190,
+                    1345
+                ],
+                "Q2": [
+                    80027,
+                    -1475,
+                    1345
+                ],
+                "Q3": [
+                    78487,
+                    -1540,
+                    1296
+                ]
+            },
+            "Change in amort cost USTs": {
+                "Q1": null,
+                "Q2": -49,
+                "Q3": null
+            }
+        }
+    }
+}
